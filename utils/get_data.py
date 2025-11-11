9c598c5cbf73c434541058169d1489a4d52c4192
Ahmed-Sarboudine <thamizsarb@gmail.com>
2025-11-11 11:56:41 +0100

    projetUnesco/panda/add-function: load_unesco_data

---
+++ utils/get_data.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+
+def load_unesco_data():
+    # Récupération du csv à partir de l'arborecence du projet
+    base_dir = os.path.dirname(os.path.dirname(__file__))
+    data_path = os.path.join(base_dir, "data", "whc-sites-2019 - whc-sites-2019.csv")
+
+    # Lecture du fichier CSV avec pandas
+    data_frame = pd.read_csv(data_path)
+
+    return data_frame
+
+
+if __name__ == "__main__":
+    iris_df = load_unesco_data()
+    print(iris_df.head())
+    print("\nColonnes disponibles :", iris_df.columns.tolist())
